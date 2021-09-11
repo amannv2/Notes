4e9f07da1ba1e9c815af03093a37a335cb72959e
Aman Verma <amannverma63@gmail.com>
2021-09-11 23:15:09 +0530

    Update env.js

--- src/env.js
+++ src/env.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = "http://3.87.78.149:3000";
+  window.__env.apiUrl = "http://52.23.172.45:3000";
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
